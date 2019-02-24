a3300f21085fb4778dad8e96cb559f5f105507c2
Thomas Kuenneth <kuennetht@gmail.com>
2019-02-24 10:46:55 +0100

    new project added

---
+++ UnitTestDemo/app/src/main/java/com/thomaskuenneth/unittestdemo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.thomaskuenneth.unittestdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextGuess;
+    private TextView textViewMessage;
+    private RadioGroup difficulty;
+    private int number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        difficulty = findViewById(R.id.difficulty);
+        difficulty.check(R.id.button_easy);
+        difficulty.setOnCheckedChangeListener((group, checkedId) -> randomNumber());
+        editTextGuess = findViewById(R.id.edittext_guess);
+        textViewMessage = findViewById(R.id.textview_message);
+
+        randomNumber();
+    }
+
+    public void handleGuess(View v) {
+        int guess = Integer.parseInt(editTextGuess.getText().toString());
+        boolean correct = guess(guess);
+        if (correct) {
+            randomNumber();
+            textViewMessage.setText(R.string.correct);
+        } else {
+            textViewMessage.setText(R.string.wrong);
+        }
+        editTextGuess.setText("");
+    }
+
+    private void randomNumber() {
+        number = 1 + (int) (Math.random() * getMax());
+        textViewMessage.setText(R.string.guess_a_number);
+    }
+
+    private float getMax() {
+        float max;
+        switch (difficulty.getCheckedRadioButtonId()) {
+            case R.id.button_easy:
+                max = 3f;
+                break;
+            case R.id.button_medium:
+                max = 5f;
+                break;
+            default:
+                max = 10f;
+                break;
+        }
+        return max;
+    }
+
+    private boolean guess(int guess) {
+        return number == guess;
+    }
+}
