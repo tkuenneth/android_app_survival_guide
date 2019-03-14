1c420d24ea2d8cd86305bac645a39992b27106cd
Thomas Kuenneth <kuennetht@gmail.com>
2019-03-14 07:42:28 +0100

    work in progress

--- AndroidVersionDemo/app/src/main/java/com/thomaskuenneth/androidversiondemo/MainActivity.java
+++ AndroidVersionDemo/app/src/main/java/com/thomaskuenneth/androidversiondemo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.thomaskuenneth.androidversiondemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.AssetManager;
 import android.graphics.ImageDecoder;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Magnifier;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Locale;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,17 +24,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AssetManager am = getAssets();
-        Thread t = new Thread(() -> {
-            ImageDecoder.Source source = ImageDecoder.createSource(am, "Beanie_App-Icon.png");
-            try {
-                Drawable drawable = ImageDecoder.decodeDrawable(source);
-                ImageView view = findViewById(R.id.imageView);
-                view.setImageDrawable(drawable);
-            } catch (IOException e) {
-                Log.e(TAG, "Could not decode file", e);
-            }
-        });
+        Thread t = new Thread(this::setImage);
         t.start();
+
+        TextView version = findViewById(R.id.version);
+        version.setText(String.format(Locale.US, "Android %s",
+                Build.VERSION.RELEASE));
+        TextView apiLevel = findViewById(R.id.apiLevel);
+        apiLevel.setText(String.format(Locale.US, "API-Level %d",
+                Build.VERSION.SDK_INT));
+
+        Magnifier m = new Magnifier(null);    }
+
+    private void setImage() {
+        AssetManager am = getAssets();
+        ImageDecoder.Source source = ImageDecoder.createSource(am, "Beanie_App-Icon.png");
+        try {
+            Drawable drawable = ImageDecoder.decodeDrawable(source);
+            ImageView view = findViewById(R.id.imageView);
+            view.setImageDrawable(drawable);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not decode file", e);
+        }
     }
 }
