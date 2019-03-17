d062aebe0d71763c9f6dd1be6a1d959af5ccef16
Thomas Künneth <kuennetht@gmail.com>
2019-03-17 12:23:04 +0100

    Migrated to AndroidX

--- UnitTestDemo/app/src/androidTest/java/com/thomaskuenneth/unittestdemo/MainActivityTest.java
+++ UnitTestDemo/app/src/androidTest/java/com/thomaskuenneth/unittestdemo/MainActivityTest.java
@@ -1,17 +1,17 @@
 package com.thomaskuenneth.unittestdemo;
 
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -20,7 +20,8 @@ public class MainActivityTest {
     // https://developer.android.com/training/testing/espresso/setup
 
     @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void test1() {
