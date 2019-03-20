adfcdff0685da55aba0d747310a5b53a4825fa6c
Thomas Künneth <kuennetht@gmail.com>
2019-03-20 12:27:39 +0100

    UIThreadDemo added

---
+++ UIThreadDemo/app/src/main/java/com/thomaskuenneth/uithreaddemo/UIThreadDemo.java
@@ -0,0 +1,37 @@
+package com.thomaskuenneth.uithreaddemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class UIThreadDemo extends Activity {
+
+    public static final String TAG = UIThreadDemo.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final TextView tv = findViewById(R.id.textview);
+        final CheckBox checkbox = findViewById(R.id.checkbox);
+        checkbox.setOnCheckedChangeListener((buttonView, isChecked) -> tv.setText(Boolean.toString(isChecked)));
+        checkbox.setChecked(true);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            tv.setText(UIThreadDemo.this.getString(R.string.begin));
+            if (checkbox.isChecked()) {
+                try {
+                    Thread.sleep(3500);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "sleep()", e);
+                }
+            } else {
+                while (true) ;
+            }
+            tv.setText(UIThreadDemo.this.getString(R.string.end));
+        });
+    }
+}
